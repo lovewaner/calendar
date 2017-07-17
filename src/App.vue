9f7864a35c75250a95d3e74c1fb957b73c5275ab
qingnilaichi <qing474973903@163.com>
2017-07-17 11:21:23 +0800

    日历组件

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+<style scoped>
+#app{
+	width: 500px;
+	margin: 100px auto 0;
+}
+</style>
+
