65fddf01a83a51008073c0cba068f66383daa75a
qingnilaichi <qing474973903@163.com>
2017-07-27 14:23:11 +0800

    book

--- src/components/editData.vue
+++ src/components/editData.vue
@@ -11,6 +11,7 @@
         <div class="editContent">
             <input type="text" v-model="editValue">
             <button>确定</button>
+            我是个大帅比
         </div>
         <table>
             <thead>
